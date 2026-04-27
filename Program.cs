45bdf26d6d1084caee691c6fb20db01d7fda7a93
zaytsev-kir <izhgulin.aleksandar@yandex.ru>
2026-04-27 17:31:16 +0300

    Добавлена первая программа

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class HelloGit
+{
+    static void Main()
+    {
+        Console.WriteLine("Привет, Git-сервис!");
+        Console.WriteLine($"Дата: {DateTime.Now:dd.MM.yyyy}");
+    }
+}
